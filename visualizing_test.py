fede638b5a05394249c685e8b40f1e7b426b763d
headcubicle <headcubicle@gmail.com>
2018-01-02 10:27:31 +0000

    create plot_histogram_local module

--- visualizing_test.py
+++ visualizing_test.py
@@ -1,49 +1,9 @@
 import sys
-from collections import Counter
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-#%matplotlib inline
-import numpy as np
-from scipy import linalg as la
 
 from qiskit import QuantumCircuit, QuantumProgram
 import Qconfig
 
-from qiskit.tools.visualization import plot_histogram, plot_state
-
-def plot_histogram_local(filename, data, number_to_keep=False):
-    """Plot a histogram of data.
-    data is a dictionary of  {'000': 5, '010': 113, ...}
-    number_to_keep is the number of terms to plot and rest is made into a
-    single bar called other values
-    """
-    if number_to_keep is not False:
-        data_temp = dict(Counter(data).most_common(number_to_keep))
-        data_temp["rest"] = sum(data.values()) - sum(data_temp.values())
-        data = data_temp
-
-    labels = sorted(data)
-    values = np.array([data[key] for key in labels], dtype=float)
-    pvalues = values / sum(values)
-    numelem = len(values)
-    ind = np.arange(numelem)  # the x locations for the groups
-    width = 0.35  # the width of the bars
-    fig, ax = plt.subplots()
-    rects = ax.bar(ind, pvalues, width, color='seagreen')
-    # add some text for labels, title, and axes ticks
-    ax.set_ylabel('Probabilities', fontsize=12)
-    ax.set_xticks(ind)
-    ax.set_xticklabels(labels, fontsize=12, rotation=70)
-    ax.set_ylim([0., min([1.2, max([1.2 * val for val in pvalues])])])
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-                '%f' % float(height),
-                ha='center', va='bottom')
-    fig.savefig(filename)
+from plot_histogram_local import *
 
 program = QuantumProgram()
 n = 3  # number of qubits 
