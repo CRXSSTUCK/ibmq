5b2dd84592eeea91d8e802011a892f26dbaadb0a
headcubicle <headcubicle@gmail.com>
2018-01-14 12:19:09 +0000

    update random bombs sample

--- generating_random_bombs/generating_random_bombs.py
+++ generating_random_bombs/generating_random_bombs.py
@@ -56,3 +56,43 @@ for circuit in circuits:
         bombs.append(int(key))
 #print(', '.join(('Live' if bomb else 'Dud' for bomb in bombs))) # Uncomment to print out "truth" of bombs
 plot_histogram_file('bomb_generation_result.svg', Counter(('Live' if bomb else 'Dud' for bomb in bombs))) #Plotting bomb generation results
+
+device = 'local_qasm_simulator' #Running on the simulator
+circuits = ["IFM_meas"+str(i) for i in range(N)]
+#Creating one measurement circuit for each bomb
+for i in range(N):
+    bomb = bombs[i]
+    q = Q_program.get_quantum_register("q")
+    c = Q_program.get_classical_register('c')
+    IFM = Q_program.create_circuit(circuits[i], [q], [c])
+    for step in range(steps):
+        IFM.ry(eps, q[0]) #First we rotate the control qubit by epsilon
+        if bomb: #If the bomb is live, the gate is a controlled X gate
+            IFM.cx(q[0],q[1])
+        #If the bomb is a dud, the gate is a controlled identity gate, which does nothing
+        IFM.measure(q[1], c[step]) #Now we measure to collapse the combined state
+    IFM.measure(q[0], c[steps])
+Q_program.get_qasms(circuits)
+result = Q_program.execute(circuits, device, shots=1, max_credits=5, wait=10, timeout=240)
+
+def get_status(counts):
+    # Return whether a bomb was a dud, was live but detonated, or was live and undetonated
+    # Note that registers are returned in reversed order
+    for key in counts:
+        if '1' in key[1:]:
+             #If we ever measure a '1' from the measurement qubit (q1), the bomb was measured and will detonate
+            return '!!BOOM!!'
+        elif key[0] == '1':
+            #If the control qubit (q0) was rotated to '1', the state never entangled because the bomb was a dud
+            return 'Dud'
+        else:
+            #If we only measured '0' for both the control and measurement qubit, the bomb was live but never set off
+            return 'Live'
+
+
+results = {'Live': 0, 'Dud': 0, "!!BOOM!!": 0}
+for circuit in circuits:
+    status = get_status(result.get_counts(circuit))
+    results[status] += 1
+plot_histogram_file('testing_the_bombs.svg', results)
+
